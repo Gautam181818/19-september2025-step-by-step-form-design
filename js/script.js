13ba5155ed741d7db974982f587fb3c44278207b
gautam777777 <gautamsharma0057@gmail.com>
2025-09-22 18:04:20 +0530

    js file added

---
+++ js/script.js
@@ -0,0 +1,63 @@
+console.log("hello")
+
+alert("hiii")
+
+console.log("hello")
+
+name = "virat"
+console.log(name)
+
+age = 24
+console.log(age)
+
+x = null;
+y = undefined;
+
+console.log(x);
+console.log(y);
+
+{
+    let a = 6;
+    console.log(a);
+}
+
+const student = {
+    name : "virat",
+    age : 36,
+    isPass : true
+};
+
+student["age"] = student["age"] + 1
+
+console.log(student["name"]);
+console.log(student.isPass);
+console.log(student.age);
+
+const pen = {
+    name : "Tri-Max",
+    Price : 50,
+    rating : 4,
+    offer : 5
+};
+
+console.log(pen);
+
+const profile = {
+    userName : "Virat.Kohli",
+    followers : 273 + " " + "Million",
+    following : 284,
+    isFollow : true,
+    post : 1037,
+    bio : "Carpediem!"
+}
+
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile.userName);
+console.log(typeof profile.followers);
+console.log(typeof profile.following);
+console.log(typeof profile.isFollow);
+console.log(typeof profile.post);
+console.log(typeof profile.bio);
+
+
